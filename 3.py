3e862f7cabe1d28b16e1f91e15dd982cffb37a4b
Srinathprasanna98 <52488844+Srinathprasanna98@users.noreply.github.com>
2019-07-03 15:21:30 +0530

    Update and rename 3.py to 4.py

--- 3.py
+++
@@ -1,9 +0,0 @@
-a=input()
-b='aeiouAEIOU'
-for i in b:
-    if(i==a):
-        print("Vowel")
-        break
-    else:
-        print("Consonant")
-        break
---
+++ 4.py
@@ -0,0 +1,11 @@
+a=input()
+b='aeiouAEIOU'
+c="QWRTYPLKJHGFDSZXCVBNMqwrytyplkjhgfdszxcvbnm"
+if(a in b):
+    print("Vowel")
+elif(a in c):
+    print("Consonant")
+else:
+    print("Invalid")
+    
+ 
